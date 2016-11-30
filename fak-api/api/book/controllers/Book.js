074cf49b27965e3b109a1ab99465975c283f26de
KuongKnight <kuongknight.bk@gmail.com>
2016-11-30 16:15:46 +0700

    Initial commit

---
+++ fak-api/api/book/controllers/Book.js
@@ -0,0 +1,122 @@
+'use strict';
+
+const model = 'book';
+
+/**
+ * A set of functions called "actions" for `book`
+ */
+
+module.exports = {
+
+  /**
+   * Get book entries.
+   *
+   * @return {Object|Array}
+   */
+
+  find: function * () {
+    this.model = model;
+    try {
+      let entries = yield strapi.hooks.blueprints.find(this);
+      this.body = entries;
+    } catch (err) {
+      this.body = err;
+    }
+  },
+
+  /**
+   * Get a specific book.
+   *
+   * @return {Object|Array}
+   */
+
+  findOne: function * () {
+    this.model = model;
+    try {
+      let entry = yield strapi.hooks.blueprints.findOne(this);
+      this.body = entry;
+    } catch (err) {
+      this.body = err;
+    }
+  },
+
+  /**
+   * Create a book entry.
+   *
+   * @return {Object}
+   */
+
+  create: function * () {
+    this.model = model;
+    try {
+      let entry = yield strapi.hooks.blueprints.create(this);
+      this.body = entry;
+    } catch (err) {
+      this.body = err;
+    }
+  },
+
+  /**
+   * Update a book entry.
+   *
+   * @return {Object}
+   */
+
+  update: function * () {
+    this.model = model;
+    try {
+      let entry = yield strapi.hooks.blueprints.update(this);
+      this.body = entry;
+    } catch (err) {
+      this.body = err;
+    }
+  },
+
+  /**
+   * Destroy a book entry.
+   *
+   * @return {Object}
+   */
+
+  destroy: function * () {
+    this.model = model;
+    try {
+      let entry = yield strapi.hooks.blueprints.destroy(this);
+      this.body = entry;
+    } catch (err) {
+      this.body = err;
+    }
+  },
+
+  /**
+   * Add an entry to a specific book.
+   *
+   * @return {Object}
+   */
+
+  add: function * () {
+    this.model = model;
+    try {
+      let entry = yield strapi.hooks.blueprints.add(this);
+      this.body = entry;
+    } catch (err) {
+      this.body = err;
+    }
+  },
+
+  /**
+   * Remove a specific entry from a specific book.
+   *
+   * @return {Object}
+   */
+
+  remove: function * () {
+    this.model = model;
+    try {
+      let entry = yield strapi.hooks.blueprints.remove(this);
+      this.body = entry;
+    } catch (err) {
+      this.body = err;
+    }
+  }
+};
